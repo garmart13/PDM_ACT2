a53cf9220c77a406514be6d931238f63924c72ae
Mimish Vader13 <garmart13>
2018-09-19 18:01:55 -0500

    se cambio el texto del TV

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -5,36 +5,14 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView campo_texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Actividad 1 Introduccion a TextView.
-        //https://developer.android.com/reference/android/widget/TextView
-        //TextView es un componente de Android que permite desplegar texto.
-
-        // El archivo del layout de la actividad se encuentra en la carpeta
-        // res/layout/activity_main.xml
-        setContentView(R.layout.activity_main);
-
-
-        //Para hacer referencia a un componente del layout, utilizamos el metodo findViewById
-        //y le pasamos como parametro el id correspondiente.
-        //Para hacer referencia al Textview con id tv
-        TextView tv = findViewById(R.id.tv);
-
-
-        //Para cambiar el texto de tv
-        tv.setText("Hola mundo");
-
-        //Actividades
-        //a) Modifica el texto "Hola mundo", pon tu nombre y ejecuta la aplicacion
-        //b) Crea una entrada en la carpeta res/values/strings.xml con tu numero de cuenta
-        //c) modifica setText para que lea esta entrada de texto (Tip, puedes hacer referencia al texto con R.string.<id entrada>)
-        //d) Ejecuta la aplicacion
-        //e) Sube tu codigo al repositorio.
-        //f) Sube un documento en word a la plataforma Moodle con las capturas de pantalla de tu actividad. Incluye la liga a tu repositorio
+        campo_texto = (TextView) findViewById(R.id.texto);
+        campo_texto.setText("Tu nombre es Nayelly y el numero de cuenta 416129924");
 
     }
 }
